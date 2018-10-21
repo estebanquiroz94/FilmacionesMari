43480091cb80f8c287da52a8fc5e1cb872d64696
Juan Quiroz <jesteban.quiroz@gmail.com>
2018-10-21 09:06:08 -0500

    Se agrega proyecto de Infraetructura para realizar la administración del modelo de BD y el contexo de aplicación.

---
+++ FirlmacionesMari.Infraestructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirlmacionesMari.Infraestructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
